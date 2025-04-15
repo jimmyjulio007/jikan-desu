75d5f51a22dcbc421ef65b442d162d7e2579b336
jimmyjulio007 <jimmyjulio100@gmail.com>
2025-04-16 02:03:10 +0300

    image configuration hostname

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["cdn.myanimelist.net"],
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "cdn.myanimelist.net/anime/",
-
-        pathname: "/**",
+        hostname: "cdn.myanimelist.net",
+        pathname: "/images/anime/**",
+        
       },
     ],
   }
